b694f92c3f7263605e876d315929d154264fa1a2
luhaifeng <lhf222458@ncarzone.com>
2022-03-25 10:54:32 +0800

    feat: add '-e/--empty' operation to delete all addresses

--- src/utils/etl.js
+++ src/utils/etl.js
@@ -1,14 +1,12 @@
 #!/usr/bin/env zx
 
 require('zx/globals')
-const { errorHandler, warningHandler } = require('./common')
+const { errorHandler, warningHandler, successHandler } = require('./common')
 const fs = require('fs')
 const path = require('path')
 const { PLATFORMS_TYPE } = require('../constants/index.js')
 
 const BASE_URL = path.join(process.env.HOME, '/gla.json')
-
-module.exports.BASE_URL = BASE_URL
 
 /**
  * jumpUrl
@@ -37,6 +35,16 @@ module.exports.getAddresses = function (cb, isAdd = false) {
       cb(data)
     } catch (err) {
       errorHandler(`Get addresses error: ${err}`)
+    }
+  })
+}
+
+module.exports.addAddresses = function (data, { successMsg = 'Completed!', errorMsg = 'Error!'}) {
+  fs.writeFile(BASE_URL, JSON.stringify(data), err => {
+    if (err) {
+      errorHandler(`${errorMsg}${err}`)
+    } else {
+      successHandler(successMsg)
     }
   })
 }
