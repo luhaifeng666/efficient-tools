e9f27d345184f04e15c94ce2ca5ca689bb849cb3
haifeng.lu <haifeng.lu@ly.com>
2022-08-02 16:45:59 +0800

    feat: improve the function

--- bin/etp.js
+++ bin/etp.js
@@ -9,25 +9,33 @@
 const dotenv = require('dotenv')
 const tinify = require('tinify')
 const path = require('path')
-const fs = require('fs')
+const { readdir, mkdir } = require('fs/promises')
 const { version } = require('../package.json')
 const { program } = require('../src/utils/programInit')
 const { promptCreator } = require('../src/utils/etl')
 const { successHandler, errorHandler, handleDotenv, handleDotenvCheck, notEmpty, promisify } = require('../src/utils/common')
 
-const dotenvPath = path.join(__dirname, '../.tiny.env')
+// dotenv configuration
+const dotenvPath = path.join(__dirname, '../.env')
 dotenv.config({ path: dotenvPath })
+
+// exts
+const IMAGE_SUPPORT_EXTS = [
+  '.webp', '.jpeg', '.jpg', '.png'
+]
+
+// PWD
+const BASE_URL = process.env.PWD
 
 program.version(version, '-v, --version')
 
 program
   .option('-s, --set', 'set your API key')
   .option('-c, --compress', 'image compression')
-  .option('-a, --all', 'compress all images in the current directory')
 
 program.parse(process.argv)
 
-const { set, compress, all } = program.opts()
+const { set, compress } = program.opts()
 
 // init tinify key
 function init (key = process.env.TINIFY_API_KEY ) {
@@ -35,10 +43,55 @@ function init (key = process.env.TINIFY_API_KEY ) {
 }
 
 // get all images' name
-function getAllImagesName () {
-
+async function getAllImagesName (isDeep, dir = BASE_URL) {
+  const res = {}
+  try {
+    const { name } = path.parse(dir)
+    const hiddenFileRule = /(^|\/)\.[^\/\.]/g
+    const files = (await readdir(dir)).filter(file => !hiddenFileRule.test(file))
+    if (files.length) {
+      res[name] = {
+        imageNames: files.filter(file => IMAGE_SUPPORT_EXTS.includes(path.extname(`${dir}/${file}`).toLowerCase()))
+      }
+      if (isDeep) {
+        const dirs = files.filter(file => !path.extname(`${dir}/${file}`))
+        if (dirs.length) {
+          const handlers = dirs.map(dirname => getAllImagesName(isDeep, `${dir}/${dirname}`))
+          const data = await Promise.allSettled(handlers)
+          res[name].children = data.reduce((obj, { value }) => ({...obj, ...value}), {})
+        }
+      }
+    }
+  } catch (e) {
+    errorHandler(e.message)
+  }
+  return res
 }
 
+// compress handler
+async function compressHandler(baseUrl, images, originBaseUrl) {
+  const dirs = Object.keys(images)
+  await Promise.allSettled(dirs.map(dir => mkdir(`${baseUrl}/${dir}`)))
+  dirs.forEach(async dir => {
+    const { imageNames, children } = images[dir]
+    const dirname =`${baseUrl}/${dir}`
+
+    imageNames.forEach(name => {
+      const imagePath = `${originBaseUrl}/${dir}/${name}`
+      promisify((resolve, reject) => {
+        const source = tinify.fromFile(imagePath)
+        resolve(source.toFile(`${dirname}/${name}`))
+      }).then(() => {
+        successHandler(`${imagePath} compression completed!`);
+      })
+    })
+    if (children) {
+      await compressHandler(dirname, children, `${originBaseUrl}/${dir}`)
+    }
+  })
+}
+
+// initialization
 init()
 
 // set API key
@@ -75,12 +128,41 @@ if (compress) {
         // process accroding to selection
         switch (answer.type) {
           case 'local-images':
-            try {
-              const source = tinify.fromFile("src")
-              source.toFile("test.png")
-            } catch (e) {
-              errorHandler(e)
-            }
+            promptCreator([
+              {
+                type: 'confirm',
+                name: 'isDeep',
+                message: 'Shallow or depth(default shallow)?',
+                default: false
+              },
+              {
+                type: 'input',
+                name: 'pathname',
+                message: 'Please type the target directory name(absolute path):',
+                default: BASE_URL
+              },
+              {
+                type: 'input',
+                name: 'dName',
+                message: 'Please type the directory name used to store compressed images:',
+                default: new Date().getTime()
+              }
+            ], async answer => {
+              try {
+                const { isDeep, dName, pathname } = answer
+                const images = await getAllImagesName(isDeep, pathname)
+                if (Object.keys(images).length) {
+                  // create a new directory to store compressed images
+                  const baseUrl = path.resolve(pathname, `../`)
+                  await mkdir(`${baseUrl}/${dName}`)
+                  // compress images
+                  await compressHandler(`${baseUrl}/${dName}`, images, baseUrl)
+                }
+              } catch (e) {
+                errorHandler(e)
+              }
+            })
+            break
           case 'online-images':
             promptCreator([
               {
