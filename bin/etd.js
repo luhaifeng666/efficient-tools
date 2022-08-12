b62b6e9398a9502ec4c7def1e1640c443a00bc90
luhaifeng666 <youzui@hotmail.com>
2022-08-12 17:53:30 +0800

    build: add eslint

--- bin/etd.js
+++ bin/etd.js
@@ -1,5 +1,7 @@
 #! /usr/bin/env node
 
+/*global process */
+
 /**
  * efficient-tools-dictionary
  * An English-Chinese translation dictionary
@@ -15,60 +17,60 @@ import { APP_ID, SECRET_KEY, LANGUAGES } from '../src/constants/index.js'
 dotenvInit()
 
 program
-  .option('-c, --config', 'add your appId and secret')
-  .option('-l, --language', 'list all supported languages')
-  .option('-t, --translate', 'translate')
-  .option('-s, --set', 'set translation rule(from A to B)')
-  .option('-r, --rule', 'print current translation rule')
+	.option('-c, --config', 'add your appId and secret')
+	.option('-l, --language', 'list all supported languages')
+	.option('-t, --translate', 'translate')
+	.option('-s, --set', 'set translation rule(from A to B)')
+	.option('-r, --rule', 'print current translation rule')
 
 program.parse(process.argv)
 
 const { config, language, translate, set, rule } = program.opts()
 
 function truncate (q) {
-  const len = q.length
-  if(len <= 20) return q
-  return q.substring(0, 10) + len + q.substring(len - 10, len)
+	const len = q.length
+	if(len <= 20) return q
+	return q.substring(0, 10) + len + q.substring(len - 10, len)
 }
 
 // get language key/name map
 function getLanguageMap () {
-  return Object.keys(LANGUAGES).reduce((obj, key) => ({
-    ...obj,
-    [LANGUAGES[key]]: key
-  }), {})
+	return Object.keys(LANGUAGES).reduce((obj, key) => ({
+		...obj,
+		[LANGUAGES[key]]: key
+	}), {})
 }
 
 // get from & to
 function getFromAndTo () {
-  const from = process.env.FROM || 'zh-CHS'
-  const to = process.env.TO || 'en'
-  return {
-    from, to
-  }
+	const from = process.env.FROM || 'zh-CHS'
+	const to = process.env.TO || 'en'
+	return {
+		from, to
+	}
 }
 
 async function handleTranslateReq (q) {
-  const appKey = process.env.APP_ID
-  const key = process.env.SECRET_KEY
-  const salt = new Date().getTime()
-  const curtime = Math.round(new Date().getTime() / 1000)
-  const { from, to } = getFromAndTo()
-  const str1 = appKey + truncate(q) + salt + curtime + key
-  const sign = CryptoJS.SHA256(str1).toString(CryptoJS.enc.Hex)
-  let data = {}
+	const appKey = process.env.APP_ID
+	const key = process.env.SECRET_KEY
+	const salt = new Date().getTime()
+	const curtime = Math.round(new Date().getTime() / 1000)
+	const { from, to } = getFromAndTo()
+	const str1 = appKey + truncate(q) + salt + curtime + key
+	const sign = CryptoJS.SHA256(str1).toString(CryptoJS.enc.Hex)
+	let data = {}
 
-  try {
-    const res = await axios({
-      method: 'get',
-      url: 'http://openapi.youdao.com/api',
-      params: { q, appKey, salt, from, to, sign, curtime, signType: 'v3' }
-    })
-    data = res.data || {}
-  } catch (e) {
-    errorHandler(e.message || 'No results!')
-  }
-  return data
+	try {
+		const res = await axios({
+			method: 'get',
+			url: 'http://openapi.youdao.com/api',
+			params: { q, appKey, salt, from, to, sign, curtime, signType: 'v3' }
+		})
+		data = res.data || {}
+	} catch (e) {
+		errorHandler(e.message || 'No results!')
+	}
+	return data
 }
 
 /**
@@ -76,113 +78,113 @@ async function handleTranslateReq (q) {
  * @param {Object} data 
  */
 function handleParse(data) {
-  try {
-    const { query, translation, web, basic } = data
-    let res = `${query}: ${translation.toString()}`
-    if (basic) {
-      const { FROM, TO } = process.env
-      if (FROM === 'en' && TO === 'zh-CHS') {
-        res = `${query}: ${basic['us-phonetic'] ? `us: [${basic['us-phonetic']}]` : ''} ${basic['uk-phonetic'] ? `uk: [${basic['uk-phonetic']}]`  : ''}
+	try {
+		const { query, translation, web, basic } = data
+		let res = `${query}: ${translation.toString()}`
+		if (basic) {
+			const { FROM, TO } = process.env
+			if (FROM === 'en' && TO === 'zh-CHS') {
+				res = `${query}: ${basic['us-phonetic'] ? `us: [${basic['us-phonetic']}]` : ''} ${basic['uk-phonetic'] ? `uk: [${basic['uk-phonetic']}]`  : ''}
 ${basic.explains.toString()}`
-      } else if (FROM === 'zh-CHS' && TO === 'en') {
-        res = `${query}: [${basic.phonetic || ''}]
+			} else if (FROM === 'zh-CHS' && TO === 'en') {
+				res = `${query}: [${basic.phonetic || ''}]
 ${basic.explains.toString()}`
-      }
-    }
-    successHandler(`------------------Translation Result------------------
+			}
+		}
+		successHandler(`------------------Translation Result------------------
 ${res}
 `)
-    if (web) {
-      successHandler(`------------------Other Explanations------------------`)
-      web.forEach(item => {
-        successHandler(`${item.key} -> ${item.value.toString()}`)
-      })
-    }
-  } catch(error) {
-    errorHandler(error.message || 'No results!')
-  }
+		if (web) {
+			successHandler('------------------Other Explanations------------------')
+			web.forEach(item => {
+				successHandler(`${item.key} -> ${item.value.toString()}`)
+			})
+		}
+	} catch(error) {
+		errorHandler(error.message || 'No results!')
+	}
 }
 
 // add appId && secret
 if (config) {
-  successHandler(`
+	successHandler(`
     **** TIP
     You need to go to https://ai.youdao.com/#/ to register first!!
     ****
   `)
-  promptCreator([
-    {
-      type: 'input',
-      name: 'APP_ID',
-      message: 'Please enter your appId',
-      validate: notEmpty('The appId shouldn\'t be empty!')
-    }, {
-      type: 'input',
-      name: 'SECRET_KEY',
-      message: 'Please enter your secret-key',
-      validate: notEmpty('The secret-key shouldn\'t be empty!')
-    },
-  ], answer => {
-    handleDotenv(answer)
-  })
+	promptCreator([
+		{
+			type: 'input',
+			name: 'APP_ID',
+			message: 'Please enter your appId',
+			validate: notEmpty('The appId shouldn\'t be empty!')
+		}, {
+			type: 'input',
+			name: 'SECRET_KEY',
+			message: 'Please enter your secret-key',
+			validate: notEmpty('The secret-key shouldn\'t be empty!')
+		},
+	], answer => {
+		handleDotenv(answer)
+	})
 }
 
 // list all supported languages
 if (language) {
-  console.table(LANGUAGES)
+	console.table(LANGUAGES)
 }
 
 // translate
 if (translate) {
-  handleDotenvCheck([APP_ID, SECRET_KEY], res => {
-    if (res) {
-      promptCreator([
-        {
-          type: 'input',
-          name: 'question',
-          message: 'What do you wanna translate?',
-          validate: notEmpty('The translation content shouldn\'t be empty！')
-        }
-      ], async answer => {
-        const data = await handleTranslateReq(answer.question)
-        handleParse(data)
-      })
-    } else {
-      errorHandler('appId or secret-key shouldn\'t be empty! Please config them first by "etd -c/--config" command.')
-    }
-  })
+	handleDotenvCheck([APP_ID, SECRET_KEY], res => {
+		if (res) {
+			promptCreator([
+				{
+					type: 'input',
+					name: 'question',
+					message: 'What do you wanna translate?',
+					validate: notEmpty('The translation content shouldn\'t be empty！')
+				}
+			], async answer => {
+				const data = await handleTranslateReq(answer.question)
+				handleParse(data)
+			})
+		} else {
+			errorHandler('appId or secret-key shouldn\'t be empty! Please config them first by "etd -c/--config" command.')
+		}
+	})
 }
 
 // set
 if (set) {
-  const choices = Object.keys(LANGUAGES).map(key => `${key}/${LANGUAGES[key]}`)
-  const { from, to } = getFromAndTo()
-  const languageMap = getLanguageMap()
-  promptCreator([
-    {
-      type: 'list',
-      name: 'FROM',
-      message: 'Which kind of language do you wanna translate?',
-      choices,
-      default: choices.indexOf(`${languageMap[from]}/${from}`)
-    }, {
-      type: 'list',
-      name: 'TO',
-      message: 'What language do you want to translate into?',
-      choices,
-      default: choices.indexOf(`${languageMap[to]}/${to}`)
-    }
-  ], answer => {
-    handleDotenv(Object.keys(answer).reduce((obj, key) => ({
-      ...obj,
-      [key]: answer[key].split('/')[1]
-    }), {}))
-  })
+	const choices = Object.keys(LANGUAGES).map(key => `${key}/${LANGUAGES[key]}`)
+	const { from, to } = getFromAndTo()
+	const languageMap = getLanguageMap()
+	promptCreator([
+		{
+			type: 'list',
+			name: 'FROM',
+			message: 'Which kind of language do you wanna translate?',
+			choices,
+			default: choices.indexOf(`${languageMap[from]}/${from}`)
+		}, {
+			type: 'list',
+			name: 'TO',
+			message: 'What language do you want to translate into?',
+			choices,
+			default: choices.indexOf(`${languageMap[to]}/${to}`)
+		}
+	], answer => {
+		handleDotenv(Object.keys(answer).reduce((obj, key) => ({
+			...obj,
+			[key]: answer[key].split('/')[1]
+		}), {}))
+	})
 }
 
 // print current translation rule
 if (rule) {
-  const { from, to } = getFromAndTo()
-  const languageMap = getLanguageMap()
-  successHandler(`The current translation rule is: from ${languageMap[from]}/${from} to ${languageMap[to]}/${to}.`)
+	const { from, to } = getFromAndTo()
+	const languageMap = getLanguageMap()
+	successHandler(`The current translation rule is: from ${languageMap[from]}/${from} to ${languageMap[to]}/${to}.`)
 }
--- src/utils/etl.js
+++ src/utils/etl.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env zx
 
+/*global process, $ */
+
 /*
  * @Author: luhaifeng666 youzui@hotmail.com
  * @Date: 2022-04-15 10:58:22
  * @LastEditors: luhaifeng666
- * @LastEditTime: 2022-08-11 15:16:23
+ * @LastEditTime: 2022-08-12 17:49:52
  * @Description: 
  */
 
@@ -28,8 +30,8 @@ export const BASE_URL = path.join(ETL_DIRECTORY || HOME, '/etl.json')
  * @param {String} url target url
  */
 export const jumpUrl = async function (url) {
-  let handleType = PLATFORMS_TYPE[process.platform]
-  await $`${handleType} ${url}`
+	let handleType = PLATFORMS_TYPE[process.platform]
+	await $`${handleType} ${url}`
 }
 
 /**
@@ -38,20 +40,20 @@ export const jumpUrl = async function (url) {
  * @param {Boolean} isAdd add tag
  */
 export const getAddresses = function (cb, isAdd = false) {
-  fs.readFile(BASE_URL, { encoding: 'utf-8'}, (err, data) => {
-    try {
-      if (!isAdd && err) {
-        if (err.code === 'ENOENT') {
-          warningHandler(`There\'s no address in ${BASE_URL}! Try using 'add' command.`)
-          return
-        }
-        throw err
-      }
-      cb(data)
-    } catch (err) {
-      errorHandler(`Get addresses error: ${err}`)
-    }
-  })
+	fs.readFile(BASE_URL, { encoding: 'utf-8'}, (err, data) => {
+		try {
+			if (!isAdd && err) {
+				if (err.code === 'ENOENT') {
+					warningHandler(`There's no address in ${BASE_URL}! Try using 'add' command.`)
+					return
+				}
+				throw err
+			}
+			cb(data)
+		} catch (err) {
+			errorHandler(`Get addresses error: ${err}`)
+		}
+	})
 }
 
 /**
@@ -60,15 +62,15 @@ export const getAddresses = function (cb, isAdd = false) {
  * @param {Object} msgConfig success message && error message
  */
 export const addAddresses = function (data, { successMsg = 'Completed!', errorMsg = 'Error!'} = {
-  successMsg: ''
+	successMsg: ''
 }) {
-  fs.writeFile(BASE_URL, JSON.stringify(data), err => {
-    if (err) {
-      errorHandler(`${errorMsg}${err}`)
-    } else {
-      successHandler(successMsg)
-    }
-  })
+	fs.writeFile(BASE_URL, JSON.stringify(data), err => {
+		if (err) {
+			errorHandler(`${errorMsg}${err}`)
+		} else {
+			successHandler(successMsg)
+		}
+	})
 }
 
 /**
@@ -77,9 +79,9 @@ export const addAddresses = function (data, { successMsg = 'Completed!', errorMs
  * @param {Function} cb 
  */
 export const promptCreator = function (configs, cb) {
-  inquirer.prompt(configs).then(answer => {
-    cb(answer)
-  }).catch(err => {
-    errorHandler(err)
-  })
+	inquirer.prompt(configs).then(answer => {
+		cb(answer)
+	}).catch(err => {
+		errorHandler(err)
+	})
 }
